fc4905b1dac1fdcd2974cd58f7f8bf8fb0c6ade1
Louisevirtudazo <louise.virtudazo@stylebyglobal.com>
2025-02-19 23:47:15 +0800

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
-module.exports = {
-    apps: [{
-      name: 'recipe-application',
-      script: '.output/server/index.mjs',  // Path to your Nuxt server file after build
-      instances: 'max',  // Or a number of instances
-      exec_mode: 'cluster',
-      watch: false,
-      env: {
-        PORT: 3000,
-        NODE_ENV: 'production'
-      }
-    }]
-  }
+export default {
+  apps: [{
+    name: 'recipe-application',
+    script: '.output/server/index.mjs',  // Path to your Nuxt server file after build
+    instances: 'max',  // Or a number of instances
+    exec_mode: 'cluster',
+    watch: false,
+    env: {
+      PORT: 3000,
+      NODE_ENV: 'production'
+    }
+  }]
+}
